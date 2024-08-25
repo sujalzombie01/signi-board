111e0039ffbc0f9cce2136388152c3c255a5af57
sujal <singleuse335@gmail.com>
2024-08-26 01:54:42 +0530

    ds

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const background=document.getElementById("leng")
 console.log(background);
 const colorpicker=document.getElementById("colo")
 console.log(colorpicker);
+const fontsize=document.getElementById('out0');
+const save=document.getElementById("save")
 const ctx=canvas.getContext('2d');
-console.log(ctx);
-
-
+wrm=false;
 colorpicker.addEventListener('change',(e)=>{
     ctx.fillStyle=e.target.value;
     ctx.strokeStyle=e.target.value;
@@ -20,17 +20,36 @@ canvas.addEventListener('mousedown', (e)=>{
       lasty=e.offsetY;
       console.log(lastx,lasty);
 })
-canvas.addEventListener('mouseup', (e)=>{
+canvas.addEventListener('mousemove', (event)=>{
     if(wrm){
         ctx.beginPath();
         ctx.moveTo(lastx,lasty);
-        ctx.lineTo(e.offsetX,e.offsetY);
+        ctx.lineTo(event.offsetX,event.offsetY);
         ctx.stroke();
-        lastx =e.offsetX;
-        lasty =e.offsetY;
-
-        
+        lastx =event.offsetX;
+        lasty =event.offsetY;
     }
 })
+canvas.addEventListener('mouseup', (event)=>{
+ wrm=false;
 
+});
 
+background.addEventListener('change', (e)=>{
+    ctx.fillStyle=e.target.value;
+    ctx.fillRect(0,0,800,500)
+})
+fontsize.addEventListener('change', (s)=>{
+    ctx.lineWidth=s.target.value;
+    ctx.stroke();
+})
+clear.addEventListener('click', ()=>{
+    ctx.clearRect(0,0,800,500)
+})
+save.addEventListener("click", ()=>{
+    localStorage.setItem('canvasContents', canvas.toDataURL());
+            let link = document.createElement('a');
+            link.download = 'my-canvas.png';
+            link.href = canvas.toDataURL();
+            link.click();
+})
